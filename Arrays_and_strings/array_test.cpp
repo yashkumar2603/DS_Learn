43fdc8960ed230d14a8828e09349355849f312aa
yashkumar2603 <yash.2603kumar@gmail.com>
2023-03-10 09:28:22 +0530

    base_memory_address_array

---
+++ Arrays_and_strings/array_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[5];                      
+    for(int i=0; i<5; i++)
+    {
+        cin>>a[i];
+    }
+    cout<<a<<endl;                       //gives the base memory address of the memory block alloted to the array.
+    for(int i=0; i<5; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+}
