a36efe2b6413e1d7c29e17492e2f81484fdfda38
yashkumar2603 <yash.2603kumar@gmail.com>
2023-03-10 09:37:08 +0530

    array_test

--- Arrays_and_strings/array_test.cpp
+++ Arrays_and_strings/array_test.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-    int a[5];                      
+    int a[5];  
+    cout<<endl;                    
     for(int i=0; i<5; i++)
     {
         cin>>a[i];
