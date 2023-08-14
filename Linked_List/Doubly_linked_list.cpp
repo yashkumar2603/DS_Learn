885671f2572e6897ecad38ca0ca289b617832489
yashkumar2603 <yash.2603kumar@gmail.com>
2023-08-14 22:14:36 +0530

    doubly linked list

---
+++ Linked_List/Doubly_linked_list.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    int i;
+
+    first = new Node;
+    first->data = A[0];
+    first->prev = first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p)
+    {
+        cout << (p->data);
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int Length(struct Node *p)
+{
+    int len = 0;
+    while (p)
+    {
+        len++;
+        p = p->next;
+    }
+    return len;
+}
+
+int main()
+{
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+
+    cout << length(first);
+    display(first);
+    return 0;
+}
