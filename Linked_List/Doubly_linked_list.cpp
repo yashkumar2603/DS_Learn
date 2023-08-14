b6afe669bc33c9c37073846e6f2082d0d45a2c7c
yashkumar2603 <yash.2603kumar@gmail.com>
2023-08-14 23:10:15 +0530

    Doubly linked list

--- Linked_List/Doubly_linked_list.cpp
+++ Linked_List/Doubly_linked_list.cpp
@@ -50,6 +50,57 @@ int Length(struct Node *p)
     return len;
 }
 
+void Insert(struct Node *p, int index, int x)
+{
+    struct Node *t;
+    int i;
+
+    if (index < 0 || index > Length(p))
+    {
+        return;
+    }
+    if (index == 0)
+    {
+        t = new Node;
+        t->data = x;
+        t->prev = NULL;
+        t->next = first;
+        first->prev = t;
+        first = t;
+    }
+
+    else
+    {
+        for (i = 0; i < index - 1; i++)
+            p = p->next;
+        t = new Node;
+        t->data = x;
+
+        t->prev = p;
+        t->next = p->next;
+        if (p->next)
+        {
+            p->next->prev = t;
+        }
+        p->next = t;
+    }
+}
+
+void Reverse(struct Node *p)
+{
+    struct Node *temp;
+
+    while (p != NULL)
+    {
+        temp = p->next;
+        p->next = p->prev;
+        p->prev = temp;
+        p = p->prev;
+        if (p->next == NULL)
+            first = p;
+    }
+}
+
 int main()
 {
     int A[] = {10, 20, 30, 40, 50};
