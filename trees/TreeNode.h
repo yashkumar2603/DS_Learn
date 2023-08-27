6a218a7c5a758980d7db4b5219c87bcdf1217769
yashkumar2603 <yash.2603kumar@gmail.com>
2023-08-27 21:07:18 +0530

    trees basic functions

--- trees/TreeNode.h
+++ trees/TreeNode.h
@@ -8,4 +8,10 @@ class TreeNode
 public:
     T data;
     vector<TreeNode *> children;
-}
+
+    // making a constructor to save lines of code in the .cpp file.
+    TreeNode(T data)
+    {
+        this->data = data;
+    }
+};
