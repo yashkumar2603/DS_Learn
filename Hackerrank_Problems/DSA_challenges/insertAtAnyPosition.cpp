3a5a3bbc979f042c6392ea8bf7122f664b7ccd34
yashkumar2603 <yash.2603kumar@gmail.com>
2023-10-08 13:39:11 +0530

    updates

---
+++ Hackerrank_Problems/DSA_challenges/insertAtAnyPosition.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode
+{
+public:
+    int data;
+    SinglyLinkedListNode *next;
+
+    SinglyLinkedListNode(int node_data)
+    {
+        this->data = node_data;
+        this->next = nullptr;
+    }
+};
+
+class SinglyLinkedList
+{
+public:
+    SinglyLinkedListNode *head;
+    SinglyLinkedListNode *tail;
+
+    SinglyLinkedList()
+    {
+        this->head = nullptr;
+        this->tail = nullptr;
+    }
+
+    void insert_node(int node_data)
+    {
+        SinglyLinkedListNode *node = new SinglyLinkedListNode(node_data);
+
+        if (!this->head)
+        {
+            this->head = node;
+        }
+        else
+        {
+            this->tail->next = node;
+        }
+
+        this->tail = node;
+    }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode *node, string sep, ofstream &fout)
+{
+    while (node)
+    {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node)
+        {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode *node)
+{
+    while (node)
+    {
+        SinglyLinkedListNode *temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+/*
+ * Complete the 'insertNodeAtPosition' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST llist
+ *  2. INTEGER data
+ *  3. INTEGER position
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+
+int Recursive_count(SinglyLinkedListNode *p)
+{
+    int count = 0;
+    if (p == 0) // base case
+    {
+        return 0;
+    }
+    else
+        return Recursive_count(p->next) + 1; // recursive call.
+}
+
+SinglyLinkedListNode *insertNodeAtPosition(SinglyLinkedListNode *llist, int data, int index)
+{
+
+    SinglyLinkedListNode *t;
+    SinglyLinkedListNode *first;
+    first = llist;
+
+    if (index < 0 || index > Recursive_count(llist)) // invalid insertion
+        return 0;
+
+    t = new SinglyLinkedListNode(data);
+
+    if (index == 0)
+    {
+        t->next = first;
+        first = t;
+    }
+    else
+    {
+        for (int i = 0; i < index - 1; i++)
+        {
+            llist = llist->next;
+        }
+        t->next = llist->next;
+        llist->next = t;
+    }
+    return first;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList *llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++)
+    {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int data;
+    cin >> data;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode *llist_head = insertNodeAtPosition(llist->head, data, position);
+
+    print_singly_linked_list(llist_head, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist_head);
+
+    fout.close();
+
+    return 0;
+}
