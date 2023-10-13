92e362f500ea14bc541cb8cef03d52bdd1ecff8a
yashkumar2603 <yash.2603kumar@gmail.com>
2023-10-13 12:48:29 +0530

    TakeInputLevelWise_BT

--- Binary_trees/Binary_Tree_basic.cpp
+++ Binary_trees/Binary_Tree_basic.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "BinaryTreeNode.h"
+#include <queue>
 using namespace std;
 
 void printTree(BinaryTreeNode<int> *root)
@@ -45,6 +46,42 @@ BinaryTreeNode<int> *takeInput()
 
 BinaryTreeNode<int> *takeInputLevelWise()
 {
+    int rootData;
+    cout << "Enter root data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+        cout << "Enter left child of " << front->data << endl;
+        int leftChildData;
+        cin >> leftChildData;
+        if (leftChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(leftChildData);
+            front->left = child;
+            pendingNodes.push(child);
+        }
+        cout << "Enter right child of " << front->data << endl;
+        int rightChildData;
+        cin >> rightChildData;
+        if (rightChildData != -1)
+        {
+            BinaryTreeNode<int> *child = new BinaryTreeNode<int>(rightChildData);
+            front->right = child;
+            pendingNodes.push(child);
+        }
+    }
+    return root;
 }
 
 int main()
