ea7604c17c3975ae4a66651dcc1af0ca906d54a1
yashkumar2603 <yash.2603kumar@gmail.com>
2023-12-13 23:26:28 +0530

    BST

--- BST/BinarySearchTrees.cpp
+++ BST/BinarySearchTrees.cpp
@@ -1,1 +1,125 @@
 // Check Obsidian for all codes -
+struct node
+{
+    int key;
+    struct node *left, *right;
+};
+
+// A utility function to create a new BST node
+struct node *newNode(int item)
+{
+    struct node *temp = new struct node;
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Utility function to search a key in a BST
+struct node *search(struct node *root, int key)
+{
+    // Base Cases: root is null or key is present at root
+    if (root == NULL || root->key == key)
+        return root;
+
+    // Key is greater than root's key
+    if (root->key < key)
+        return search(root->right, key);
+
+    // Key is smaller than root's key
+    return search(root->left, key);
+}
+
+// A utility function to insert a new node with given key in BST
+struct node *insert(struct node *node, int key)
+{
+    // If the tree is empty, return a new node
+    if (node == NULL)
+        return newNode(key);
+
+    // Otherwise, recur down the tree
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+
+    // Return the (unchanged) node pointer
+    return node;
+}
+
+public
+void findRange(Node node, int k1, int k2)
+{
+    if (node == null || (k1 > k2))
+    {
+        return;
+    }
+    int data = node.getData();
+    if (data >= k1 && data <= k2)
+    {
+        System.out.println(data);
+    }
+    if (k1 < data)
+    {
+        findRange(node.getLeft(), k1, k2);
+    }
+    if (k2 > data)
+    {
+        findRange(node.getRight(), k1, k2);
+    }
+}
+
+public
+void findRange(Node node, int k1, int k2)
+{
+    if (node == null || (k1 > k2))
+    {
+        return;
+    }
+    int data = node.getData();
+    if (data >= k1 && data <= k2)
+    {
+        System.out.println(data);
+    }
+    if (k1 < data)
+    {
+        findRange(node.getLeft(), k1, k2);
+    }
+    if (k2 > data)
+    {
+        findRange(node.getRight(), k1, k2);
+    }
+}
+
+vector<int> *getRootToNodePath(BinaryTreeNode<int> *root, int data)
+{
+
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    if (root->data == data)
+    {
+        vector<int> *output = new vector<int>();
+        output->push_back(root->data);
+        return output;
+    }
+
+    vector<int> *leftOutput = getRootToNodePath(root->left, data);
+    if (leftOutput != NULL)
+    {
+        leftOutput->push_back(root->data);
+        return leftOutput;
+    }
+
+    vector<int> *rightOutput = getRootToNodePath(root->right, data);
+    if (rightOutput != NULL)
+    {
+        rightOutput->push_back(root->data);
+        return rightOutput;
+    }
+    else
+    {
+        return NULL;
+    }
+}
