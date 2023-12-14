e8994f9880005883a5b713960ad4f9b7d5c54b5c
yashkumar2603 <yash.2603kumar@gmail.com>
2023-12-15 00:12:16 +0530

    OOPs

--- OOPs/OOPs_basics.cpp
+++ OOPs/OOPs_basics.cpp
@@ -1,19 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Hero
+class animal
 {
-    // properties
+private:
+    int weight;
+
 public:
-    char name[100];
-    int health;
-    char level;
+    int age;
+    char ch;
+
+    void getWeight()
+    {
+        cout << weight;
+    }
+    void setWeight(int value)
+    {
+        weight = value;
+    }
 };
 
 int main()
 {
-    Hero h1;
-    cout << "size : " << sizeof(h1) << endl;
-    cout << "Health : " << h1.health << endl;
+    animal horse;
+    horse.setWeight(400);
+    horse.getWeight();
     return 0;
 }
